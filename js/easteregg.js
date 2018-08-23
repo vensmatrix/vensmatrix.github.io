f39128b749cf80590ea473e456c6a67b0181326f
vensmatrix <41247155+vensmatrix@users.noreply.github.com>
2018-08-23 18:54:06 +0800

    member,idea,quicklinks

---
+++ js/easteregg.js
@@ -0,0 +1,15 @@
+(function($) {
+
+	'use strict';
+
+	// keanu li sucks
+
+  // loser
+  var loser = function() {
+    setTimeout(function() { 
+      if($('#loser').length > 0) {
+        $('#loser').removeClass('keanu');
+      }
+    }, 1);
+  };
+  loser();
